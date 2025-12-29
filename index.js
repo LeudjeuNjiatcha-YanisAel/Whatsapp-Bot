731f96b5fcd6559603c2570d98a95527d232e734
LeudjeuNjiatcha-Yanisael <leudjeuyannick33@gmail.com>
2025-12-29 19:01:45 +0100

    Commencement

---
+++ index.js
@@ -0,0 +1,37 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const DELETE_AFTER_SECONDS = 10;
+
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: { headless: true }
+});
+
+client.on('qr', qr => {
+  qrcode.generate(qr, { small: true,scale:1 });
+  console.log('Scanne le QR code avec ton WhatsApp mobile.');
+});
+
+client.on('ready', () => {
+  console.log('Bot connecté à WhatsApp !');
+});
+
+client.on('message_create', async msg => {
+  if (!msg.fromMe) return;
+
+  const chat = await msg.getChat();
+  if (!chat.isGroup) return;
+
+  setTimeout(async () => {
+    try {
+      await msg.delete(true);
+      console.log('Message supprimé pour tout le monde');
+    } catch (error) {
+      console.log('Suppression pour tous impossible, suppression locale');
+      await msg.delete(false);
+    }
+  }, DELETE_AFTER_SECONDS * 1000);
+});
+
+client.initialize();
